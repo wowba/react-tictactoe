1718e0e586e9f0b46bbab046b305231cb3ef6700
wowba <leeyuwk54@gmail.com>
2023-03-07 02:01:25 +0900

    props 공부

---
+++ src/components/Board.js
@@ -0,0 +1,37 @@
+import React from "react"
+import Square from "./Square"
+import "./Board.css"
+
+// 클래스형 컴포넌트, ES7 익스텐션 설치시 rcc 로 작성 가능 
+export default class Board extends React.Component {
+
+  renderSquare(num) {
+    return (
+      // 아래와 같이 리액트에서는 properties를 넘겨줄 수 있다.
+      <Square props={num}/>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="status">Next Player</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    )
+  }
+}
