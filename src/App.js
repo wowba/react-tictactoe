88064b264deda4abe9e53c06537560110b3534e9
wowba <leeyuwk54@gmail.com>
2023-03-14 02:32:25 +0900

    key 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,79 @@
 import "./App.css"
+import React, {useState} from "react"
 import Board from "./components/Board";
 
 function App() {
+
+  const [history, setHistory] = useState([{squares: Array(9).fill(null)}])
+  const [isFirstPlayer, setIsFirstPlayer] = useState(true)
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]
+    for (let i = 0; i < lines.length; i++) {
+      const [a,b,c] = lines[i]
+      if (squares[a] 
+        && squares[a] === squares[b] 
+        && squares[a] === squares[c]
+      ) {
+        return squares[a]
+      }
+    }
+    return null
+  }
+
+  const current = history[history.length - 1]
+  const winner = calculateWinner(current.squares)
+
+  let status;
+  if (winner) {
+    status = 'winner:' + winner
+  } else {
+    status = `Next player : ${isFirstPlayer? "X" : "O"}`
+  }
+
+  const handleClick = (num) => {
+    const newSquares = current.squares.slice();
+
+    if(calculateWinner(newSquares) || newSquares[num]) {
+      return;
+    }
+
+    newSquares[num] = isFirstPlayer ? "X" : "O"
+    setHistory([...history, {squares: newSquares}])
+    setIsFirstPlayer(prev => !prev)
+  }
+
+  const moves = history.map((step, move) => {
+    const desc = move ?
+    'Go to move #' + move :
+    'Go to game Start'
+    return (
+      <li key={move}>
+        <button>{desc}</button>
+      </li>
+    )
+  })
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board 
+          squares={current.squares}
+          onClick={(num) => {handleClick(num)}}
+        />
       </div>
       <div className="game-info">
-        info
+        {status}
+        <ol>{moves}</ol>
       </div>
     </div>
   );
--- src/components/Board.js
+++ src/components/Board.js
@@ -2,67 +2,20 @@ import React, {useState} from "react"
 import Square from "./Square"
 import "./Board.css"
 
-const Board = () => {
-
-  const [squares, setSquares] = useState(Array(9).fill(null))
-  const [isFirstPlayer, setIsFirstPlayer] = useState(true)
-
-  const handleClick = (num) => {
-    const newSquares = squares.slice()
-
-    if(calculateWinner(newSquares) || newSquares[num]) {
-      return;
-    }
-
-    newSquares[num] = isFirstPlayer ? "X" : "O"
-    setIsFirstPlayer(prev => !prev)
-    setSquares(newSquares)
-  }
+const Board = (props) => {
 
   const renderSquare = (num) => {
     return (
       // 아래와 같이 리액트에서는 properties를 넘겨줄 수 있다.
       <Square 
-        value={squares[num]}
-        onClick={() => handleClick(num)}
+        value={props.squares[num]}
+        onClick={() => props.onClick(num)}
       />
     )
   }
 
-  const calculateWinner = (squares) => {
-    const lines = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6]
-    ]
-    for (let i = 0; i < lines.length; i++) {
-      const [a,b,c] = lines[i]
-      if (squares[a] 
-        && squares[a] === squares[b] 
-        && squares[a] === squares[c]
-      ) {
-        return squares[a]
-      }
-    }
-    return null
-  }
-
-  const winner = calculateWinner(squares)
-  let status;
-  if (winner) {
-    status = 'winner:' + winner
-  } else {
-    status = `Next player : ${isFirstPlayer? "X" : "O"}`
-  }
-
   return (
     <div>
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
